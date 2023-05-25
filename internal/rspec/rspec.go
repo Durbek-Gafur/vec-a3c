2a3117443d62277f4f9049d2789973ed68cb5818
Durbek Gafurov <durbek.gafurov@gmail.com>
2023-05-25 17:50:17 -0500

    rspec methods

---
+++ internal/rspec/rspec.go
@@ -0,0 +1,32 @@
+package rspec
+
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Resources struct {
+	CPUs float64
+	RAM  int // in MB
+}
+
+func ParseResourcesFromEnv() (*Resources, error) {
+	resources := &Resources{}
+
+	cpuStr := os.Getenv("CPUS")
+	cpus, err := strconv.ParseFloat(cpuStr, 64)
+	if err != nil {
+		return nil, err
+	}
+	resources.CPUs = cpus
+
+	ramStr := os.Getenv("RAM")
+	ram, err := strconv.Atoi(strings.TrimSuffix(ramStr, "M"))
+	if err != nil {
+		return nil, err
+	}
+	resources.RAM = ram
+
+	return resources, nil
+}
---
+++ internal/rspec/rspec_test.go
@@ -0,0 +1,47 @@
+package rspec
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+func TestGetRspec(t *testing.T) {
+	// Set up the environment
+	os.Setenv("CPUS", "0.75")
+	os.Setenv("RAM", "512M")
+
+	// Run the function
+	resources, err := GetRspec()
+	assert.NoError(t, err, "GetRspec failed")
+
+	// Check the results
+	assert.Equal(t, 0.75, resources.CPUs, "Unexpected CPUs value")
+	assert.Equal(t, 512, resources.RAM, "Unexpected RAM value")
+
+	// Clean up the environment
+	os.Unsetenv("CPUS")
+	os.Unsetenv("RAM")
+}
+
+func TestParseResourcesFromEnv(t *testing.T) {
+	// Set up the environment
+	os.Setenv("CPUS", "0.5")
+	os.Setenv("RAM", "256M")
+
+	// Run the function
+	resources, err := ParseResourcesFromEnv()
+	assert.NoError(t, err, "ParseResourcesFromEnv failed")
+
+	// Check the results
+	assert.Equal(t, 0.5, resources.CPUs, "Unexpected CPUs value")
+	assert.Equal(t, 256, resources.RAM, "Unexpected RAM value")
+
+	// Clean up the environment
+	os.Unsetenv("CPUS")
+	os.Unsetenv("RAM")
+}
+
+func GetRspec() (*Resources, error) {
+	return ParseResourcesFromEnv()
+}
